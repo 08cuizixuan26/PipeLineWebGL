576430ceb3ab06cc6a03cd45e7526e2ae869f34e
zhaogangxiao <15226975098@163.com>
2022-06-17 10:11:30 +0800

    修改管线类型

--- ruoyi-hdsw/src/main/java/com/ruoyi/hdsw/controller/GxtypeController.java
+++ ruoyi-hdsw/src/main/java/com/ruoyi/hdsw/controller/GxtypeController.java
@@ -3,6 +3,10 @@ package com.ruoyi.hdsw.controller;
 import com.ruoyi.common.utils.uuid.IdUtils;
 import com.ruoyi.hdsw.model.Gxtype;
 import com.ruoyi.hdsw.service.impl.GxtypeServiceImpl;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +16,7 @@ import java.util.Date;
 import java.util.List;
 
 @Controller
+@Api(value = "业务类型管理", description = "业务类型管理")
 @RequestMapping("/hdsw/gxtype")
 public class GxtypeController {
     @Autowired
@@ -20,7 +25,8 @@ public class GxtypeController {
     /**
      * 查找
      */
-    @RequestMapping("/select")
+    @ApiOperation("业务管理查询功能")
+    @RequestMapping(value = "/select",method = {RequestMethod.GET,RequestMethod.POST})
     @ResponseBody
     public Object select() {
         List<Gxtype> select = gxtypeService.select();
@@ -30,10 +36,12 @@ public class GxtypeController {
     /**
      * 添加
      */
-    @RequestMapping("/insert")
+    @ApiOperation("业务管理添加功能")
+    @ApiImplicitParam(name="name",value="类型",dataType="string")
+    @RequestMapping(value = "/insert",method = {RequestMethod.GET,RequestMethod.POST})
     @ResponseBody
-    public Object insert(String id, String name) {
-        id = IdUtils.fastSimpleUUID();
+    public Object insert(String name) {
+        String id = IdUtils.fastSimpleUUID();
         Date date = new Date();
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         String time = dateFormat.format(date);
@@ -44,7 +52,9 @@ public class GxtypeController {
     /**
      * 删除
      */
-    @RequestMapping("/delete")
+    @ApiOperation("业务管理删除功能")
+    @ApiImplicitParam(name="id",value="id",dataType="string")
+    @RequestMapping(value = "/delete",method = {RequestMethod.GET,RequestMethod.POST})
     @ResponseBody
     public Object delete(String id) {
         int i = gxtypeService.delete(id);
@@ -54,7 +64,11 @@ public class GxtypeController {
     /**
      * 修改
      */
-    @RequestMapping("/update")
+    @ApiOperation("业务管理修改功能")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name="id",value="id",dataType="string"),
+            @ApiImplicitParam(name="name",value="类型",dataType="string")})
+    @RequestMapping(value = "/update",method = {RequestMethod.GET,RequestMethod.POST})
     @ResponseBody
     public Object update(String id, String name) {
         Date date = new Date();
