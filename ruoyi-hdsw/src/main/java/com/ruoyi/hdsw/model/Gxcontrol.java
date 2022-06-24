59f115e320c9495da61e1afb5b54935ff18334d9
zhaogangxiao <15226975098@163.com>
2022-06-24 14:23:56 +0800

    管线控制

---
+++ ruoyi-hdsw/src/main/java/com/ruoyi/hdsw/model/Gxcontrol.java
@@ -0,0 +1,60 @@
+package com.ruoyi.hdsw.model;
+
+public class Gxcontrol {
+    private Integer gid;
+    private Integer width;
+    private String color;
+    private String visibility;
+    private String iocn;
+
+    public Integer getGid() {
+        return gid;
+    }
+
+    public void setGid(Integer gid) {
+        this.gid = gid;
+    }
+
+    public Integer getWidth() {
+        return width;
+    }
+
+    public void setWidth(Integer width) {
+        this.width = width;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getVisibility() {
+        return visibility;
+    }
+
+    public void setVisibility(String visibility) {
+        this.visibility = visibility;
+    }
+
+    public String getIocn() {
+        return iocn;
+    }
+
+    public void setIocn(String iocn) {
+        this.iocn = iocn;
+    }
+
+    @Override
+    public String toString() {
+        return "Gxcontrol{" +
+                "gid=" + gid +
+                ", width=" + width +
+                ", color='" + color + '\'' +
+                ", visibility='" + visibility + '\'' +
+                ", iocn='" + iocn + '\'' +
+                '}';
+    }
+}
